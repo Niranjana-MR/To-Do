c1175fdae6a0533f86170b5611e5cffc285de907
Niranjana M R <niranjan.mr99@gmail.com>
2021-06-22 18:58:52 +0530

    implemented storing functionality in local storage

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const addBtn = document.querySelector('#addBtn');
 
 const filterItem = document.querySelector('#todoFilter');
 
+document.addEventListener('DOMContentLoaded', displayTodos);
 addBtn.addEventListener('click', addToDo);
 todoList.addEventListener('click', (e) => action(e));
 filterItem.addEventListener('click', (e) => filterItems(e))
@@ -21,6 +22,9 @@ function addToDo() {
     todoItem.textContent = userInput.value;
 
     todoContainer.appendChild(todoItem);
+
+    //adding to localStorage
+    saveTodo(userInput.value);
 
     const checkButton = document.createElement('button');
     checkButton.classList.add('checkButton');
@@ -49,6 +53,7 @@ function action(e) {
     const item = e.target;
     if (item.classList[0] === 'deleteButton') {
         item.parentElement.classList.add('swipe');
+        removeToD0(item.parentElement);
         item.parentElement.addEventListener('transitionend', () => item.parentElement.remove());
     }
 
@@ -81,4 +86,68 @@ function filterItems(e) {
                 break;
         }
     });
+}
+
+//saving to local storage
+
+function saveTodo(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+function displayTodos() {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function (todo) {
+        const todoContainer = document.createElement('div');
+        todoContainer.classList.add('todoContainer');
+
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('todoItem');
+        //check for empty values
+        todoItem.textContent = todo;
+
+        todoContainer.appendChild(todoItem);
+
+        const checkButton = document.createElement('button');
+        checkButton.classList.add('checkButton');
+        const checkIcon = document.createElement('i');
+        checkIcon.classList.add('fas', 'fa-check');
+        checkButton.appendChild(checkIcon);
+
+        todoContainer.appendChild(checkButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('deleteButton');
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('fas', 'fa-trash');
+        deleteButton.appendChild(deleteIcon);
+
+        todoContainer.appendChild(deleteButton);
+
+        todoList.appendChild(todoContainer);
+    })
+}
+
+function removeToD0(todo){
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const item = todo.children[0].innerText;
+    todos.splice(todos.indexOf(item), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
